2c4cea6c1e47d1d3da630954200c1c1ac8285f10
sanjana441 <sanjana.kumari12102004@gmail.com>
2025-07-18 14:39:00 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+🌐 Live URLs
+User Interface: https://prescripto-frontend-bjff.onrender.com/
+Admin & Doctor Interface: https://prescripto-admin-u840.onrender.com/
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>At Prescripto, we connect you with highly qualified and experienced doctors from various specialties. Our trusted network of medical professionals includes certified practitioners, specialists, and surgeons with years of expertise in providing top-notch healthcare. Whether you need a general consultation, a specialist advice, or ongoing care, Prescripto ensures you have access to the best medical services from the comfort of your home.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 7084721408</li>
+            <li>shyambabu_jayswal@yahoo.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
